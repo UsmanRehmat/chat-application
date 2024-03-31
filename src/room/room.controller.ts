5be583071d703e4c9eff973f1f8ed95a0ca61648
Muhammad Usman <musman@MacBook-Pro.local>
2024-03-31 22:09:53 +0300

    added global exception filters and logs

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Req } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { RoomEntity } from './entity/room.entity';
@@ -9,29 +9,34 @@ import { MessageEntity } from 'src/message/entity/message.entity';
 @Controller('room')
 export class RoomController {
 
+    logger = new Logger(RoomController.name);
     constructor(private readonly roomService: RoomService){}
 
     @ApiBearerAuth()
     @Post()
     async create(@Body() createRoomDto: CreateRoomDto, @Req() request): Promise<RoomEntity> {
+        this.logger.debug('create');
         return this.roomService.createRoom(createRoomDto, request.user);
     }
 
     @ApiBearerAuth()
     @Post('/join')
     async joinRoom(@Body() joinRoomDto: JoinRoomDto, @Req() request): Promise<MessageEntity[]> {
+      this.logger.debug('joinRoom');
       return this.roomService.addUserToRoom(joinRoomDto.roomId, request?.user?.id);
     }
 
     @ApiBearerAuth()
     @Get()
     async getAllRooms(): Promise<RoomEntity[]> {
+      this.logger.debug('getAllRooms');
       return this.roomService.getAllRooms();
     }
 
     @ApiBearerAuth()
     @Get('/user')
     async getUserRooms(@Req() request): Promise<RoomEntity[]> {
+      this.logger.debug('getUserRooms');
       return this.roomService.getRoomsByUserId(request?.user?.id);
     }
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from './entity/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -6,15 +6,18 @@ import { SingInUserDto } from './dto/signin-user.dto';
 
 @Controller('user')
 export class UserController {
+    logger = new Logger(UserController.name);
     constructor(private readonly userService: UserService){}
 
     @Post()
     async create(@Body() createUserDto: CreateUserDto): Promise<{id: number}> {
+        this.logger.debug('create');
         return this.userService.createUser(createUserDto);
     }
 
   @Post('signIn')
   async login(@Body() signInUserDto: SingInUserDto) {
+    this.logger.debug('login');
     const jwt: string = await this.userService.signIn(signInUserDto);
     return {
       access_token: jwt,
